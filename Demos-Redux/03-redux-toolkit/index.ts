c7246a9d2772eba9cb07ec08f3fb2f0a488cc652
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-12-19 14:25:25 +0100

    Exercice Redux Toolkit

---
+++ Demos-Redux/03-redux-toolkit/index.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { incrementLikes, updateName } from './actions';
+import { reducer } from './reducers';
+import { likesSelector, nameSelector } from './selectors';
+
+const store = configureStore({ reducer });
+
+store.subscribe(() => {
+  console.log('likes', likesSelector(store.getState()));
+});
+
+store.subscribe(() => {
+  console.log('name', nameSelector(store.getState()));
+});
+
+store.dispatch(incrementLikes());
+store.dispatch(updateName('Toto'));
+store.dispatch(incrementLikes());
+
+
